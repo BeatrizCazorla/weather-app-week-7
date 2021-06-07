8371c3bc15ae452b560f7febf572a61883851301
Beatriz Cazorla <biacazorla@gmail.com>
2021-06-07 10:56:04 -0300

    Icon changes

--- index.js
+++ index.js
@@ -67,7 +67,8 @@ function showTemperature(response) {
     response.data.weather[0].main;
 
     let iconElement = document.querySelector("#icon");
-    iconElement.setAtribute("src", `https://openweathermap.org/img/wn/04d@2x.png`);
+    iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
   
 }
 
